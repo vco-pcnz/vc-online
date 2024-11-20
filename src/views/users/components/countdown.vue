5d2258c204a0277e3bc729fc389e84c253bcc37d
lotus <lotus@vincentcapital.cn>
2024-11-20 15:07:29 +0800

    feat:倒计时组件

---
+++ src/views/users/components/countdown.vue
@@ -0,0 +1,52 @@
+<template>
+  <a-button block class="countdown-btn" disabled>
+    {{ countdown }}
+  </a-button>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount, watch } from "vue";
+
+// TODO
+const countdown = ref(5);
+let intervalId = null;
+
+const emit = defineEmits(["update:show"]);
+const startCountdown = () => {
+  intervalId = setInterval(() => {
+    if (countdown.value > 0) {
+      countdown.value--;
+    } else {
+      stopCountdown();
+    }
+  }, 1000);
+};
+
+const stopCountdown = () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  emit("update:show", false);
+};
+
+onMounted(() => {
+  startCountdown();
+});
+
+onBeforeUnmount(() => {
+  stopCountdown();
+});
+
+watch(countdown, (val) => {
+  if (val === 0) {
+    stopCountdown();
+  }
+});
+</script>
+
+<style scoped lang="less">
+@import "@/styles/variables.less";
+.countdown-btn {
+  background-color: @clr_charcoal;
+  color: @clr_white;
+}
+</style>

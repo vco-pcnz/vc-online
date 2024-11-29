8eabbfca4de21de8a6df892d95942db0a22c720a
ellenging <ellenging@gmail.com>
2024-11-29 18:06:01 +0800

    项目信息

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -228,4 +228,12 @@ export default {
   "验证码": "Verification code",
   "选择登录账号": "Select login account",
   "请上传头像": "Please upload avatar",
+  "项目名称": "Project name",
+  "项目类型": "Project type",
+  "项目照片": "Project images",
+  "项目地址": "Project address",
+  "楼栋数": "Number of units",
+  "项目介绍": "Project background",
+  "暂无数据，无需保存": "No data, no need to save",
+  "* 存在草稿数据，请点击{0}保存": "* Draft data exists, please click {0} to save",
 }
--- src/views/process/page/two/index.vue
+++ src/views/process/page/two/index.vue
@@ -2,57 +2,90 @@
   <div>
     <a-spin :spinning="tempLoading" size="large">
       <div class="main-info-container">
-        <process-header v-if="!queryError" :step-data="processStepData" :current="2">
-          <template #left>
-            <div class="page-title">ID: {{ currentId }}</div>
-          </template>
-          <template #right>
-            <a-button type="primary" shape="round" ghost size="small">{{ t('帮助') }}?</a-button>
-          </template>
-        </process-header>
-        <a-spin :spinning="formLoading" size="large">
-          <component v-if="tempModule" :is="tempModule"></component>
-          <a-empty v-if="!tempModule && queryError" :image="simpleImage" />
-        </a-spin>
+        <div v-if="!formLoading && !pageError">
+          <process-header v-if="!queryError" :step-data="processStepData" :current="2">
+            <template #left>
+              <div class="page-title">ID: {{ infoData?.project_apply_sn || '' }}</div>
+            </template>
+            <template #right>
+              <a-button type="primary" shape="round" ghost size="small">{{ t('帮助') }}?</a-button>
+            </template>
+          </process-header>
+          <a-spin :spinning="formLoading" size="large">
+            <component
+              v-if="tempModule"
+              :is="tempModule"
+              :info-data="infoData"
+              :draft-data="draftData"
+            ></component>
+            <a-empty v-if="!tempModule && queryError" :image="simpleImage" />
+          </a-spin>
+        </div>
+        <a-empty v-if="!formLoading && pageError" :image="simpleImage" />
       </div>
     </a-spin>
   </div>
 </template>
 
 <script setup>
-  import { watch, ref } from "vue";
+  import { ref, onMounted } from "vue";
   import { useDynamicModule } from "@/hooks/useDynamicModule";
   import ProcessHeader from "../../components/ProcessHeader.vue";
   import { useI18n } from "vue-i18n";
-  import { projectApplyProjectInfo } from "@/api/process"
+  import { projectApplyProjectInfo, projectDraftInfo } from "@/api/process"
 
   const { t } = useI18n();
   const { tempLoading, tempModule, currentId, queryError, simpleImage, processStepData } = useDynamicModule('two');
 
-  const formLoading = ref(false)
+  const formLoading = ref(true)
+  const pageError = ref(false)
 
   const infoData = ref({})
   const getInfo = () => {
     formLoading.value = true
 
     projectApplyProjectInfo({
-      project_apply_sn: currentId.value
+      uuid: currentId.value
     }).then(res => {
       infoData.value = res
       formLoading.value = false
+      pageError.value = false
     }).catch(() => {
       formLoading.value = false
+      pageError.value = true
     })
   }
 
-  watch(
-    currentId,
-    (val) => {
-      if (val) {
-        getInfo()
-      }
+  // 草稿数据
+  const draftData = ref()
+  const getDraftInfo = async () => {
+    const params = {
+      draft_step: 'two'
     }
-  )
+
+    if (currentId.value) {
+      params.uuid = currentId.value
+    }
+
+    await projectDraftInfo(params).then(res => {
+      if (res.draft) {
+        const data = JSON.parse(res.draft)
+        if (data.project_images && data.project_images.length) {
+          data.project_images = data.project_images.split(',')
+        }
+        draftData.value = data
+      }
+    })
+  }
+
+  onMounted(async () => {
+    await getDraftInfo()
+    if (!currentId.value) {
+      formLoading.value = false
+    } else {
+      await getInfo()
+    }
+  })
 </script>
 
 <style lang="less" scoped>

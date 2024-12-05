8c6fb356d015ae0e04fd2301e33ce92e3eeca601
lotus <lotus@vincentcapital.cn>
2024-12-05 10:53:21 +0800

    feat:用户详情

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,39 +1,47 @@
-import { request } from "@/utils/request";
+import { request } from '@/utils/request';
 // 用户列表
 export const getUserList = (params) =>
   request({
-    url: "user/index",
-    method: "get",
+    url: 'user/index',
+    method: 'get',
     params,
   });
 
 // 添加用户
 export const addUser = (data) =>
   request({
-    url: "user/add",
-    method: "post",
+    url: 'user/add',
+    method: 'post',
     data,
   });
 
 // 编辑用户
 export const editUser = (data) =>
   request({
-    url: "user/edit",
-    method: "post",
+    url: 'user/edit',
+    method: 'post',
     data,
   });
 
 // 用户角色-选择
 export const getRoles = (params) =>
   request({
-    url: "role/sel",
-    method: "get",
+    url: 'role/sel',
+    method: 'get',
     params,
   });
 
 export const assignRole = (data) =>
   request({
-    url: "user/setRoles",
-    method: "post",
+    url: 'user/setRoles',
+    method: 'post',
     data,
+  });
+
+// 获取用户详情
+export const getUserInfo = (params) =>
+  request({
+    url: 'user/info',
+    method: 'get',
+    params,
   });

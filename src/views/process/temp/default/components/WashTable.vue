9337c83e4ba3eee14d4a05ea2eb00d4883f0131d
夏沫凝空 <425523255@qq.com>
2025-01-05 21:21:13 +0800

    反洗钱模版页面

---
+++ src/views/process/temp/default/components/WashTable.vue
@@ -0,0 +1,164 @@
+<template>
+  <div
+    class="block-item mb"
+    :class="{ checked: securityInfo.is_check && showCheck }"
+  >
+    <!-- 新增 -->
+    <security-add-edit
+      v-model:visible="addVisible"
+      :current-id="currentId"
+      :security-status="securityInfo.check_status"
+      :typeData="securityInfo.type"
+    ></security-add-edit>
+
+    <vco-process-title :title="t('抵押物信息')">
+      <div v-if="showCheck" class="flex gap-5">
+        <a-popconfirm
+          :title="t('确定通过审核吗？')"
+          :ok-text="t('确定')"
+          :cancel-text="t('取消')"
+          @confirm="checkHandle()"
+        >
+          <a-button
+            type="dark"
+            shape="round"
+            class="uppercase"
+            v-if="!securityInfo.is_check"
+          >
+            {{ t('审核') }}
+          </a-button>
+        </a-popconfirm>
+        <a-button
+          v-if="showCheckEdit"
+          type="primary"
+          shape="round"
+          class="uppercase"
+          @click="addVisible = true"
+        >
+          {{ t('添加') }}
+        </a-button>
+      </div>
+    </vco-process-title>
+
+    <div class="sys-form-content mt-5">
+      <a-form layout="vertical">
+        <a-row :gutter="24">
+          <a-col :span="8">
+            <a-form-item :label="t('总金额')">
+              <vco-number
+                :value="securityInfo.total_money"
+                :precision="2"
+                :end="true"
+              ></vco-number>
+            </a-form-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-item :label="t('抵押物价值')">
+              <vco-number
+                :value="securityInfo.total_value"
+                :precision="2"
+                :end="true"
+              ></vco-number>
+            </a-form-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-item :label="t('抵押物数量')">
+              <p class="total-count">{{ securityInfo.count }}</p>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <div v-if="showCheck" class="check-content">
+      <i class="iconfont">&#xe647;</i>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import {
+  lmAuditStatus,
+  fcAuditStatus,
+  auditLmCheckStatus,
+} from '@/api/process';
+import SecurityAddEdit from './SecurityAddEdit.vue';
+
+const emits = defineEmits(['refresh']);
+const props = defineProps({
+  currentId: {
+    type: [Number, String],
+    required: true,
+  },
+  stepType: {
+    type: Number,
+    default: 1,
+  },
+  securityInfo: {
+    type: Object,
+    default: () => {
+      return {
+        count: 0,
+        total_value: '0.00',
+        is_check: false,
+        check_status: 407,
+        type: [],
+      };
+    },
+  },
+});
+
+const { t } = useI18n();
+
+const showCheck = computed(() => {
+  return [1, 2, 4].includes(props.stepType);
+});
+
+const showCheckEdit = computed(() => {
+  return [1].includes(props.stepType);
+});
+
+const checkHandle = async () => {
+  let ajaxFn = null;
+  let params = {};
+  if (props.stepType === 1) {
+    ajaxFn = lmAuditStatus;
+    params = {
+      lm_audit_status: props.securityInfo.check_status,
+      uuid: props.currentId,
+    };
+  } else if (props.stepType === 2) {
+    ajaxFn = fcAuditStatus;
+    params = {
+      fc_audit_status: props.securityInfo.check_status,
+      uuid: props.currentId,
+    };
+  } else if (props.stepType === 4) {
+    ajaxFn = auditLmCheckStatus;
+    params = {
+      lm_check_status: props.securityInfo.check_status,
+      uuid: props.currentId,
+    };
+  }
+
+  if (ajaxFn) {
+    await ajaxFn(params)
+      .then(() => {
+        emits('refresh');
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
+  }
+};
+
+const addVisible = ref(false);
+</script>
+
+<style lang="less" scoped>
+.total-count {
+  font-size: 24px;
+}
+</style>

8e680c26dbe7612d9135bb5b7ec46d5b8feb06a8
ellenging <ellenging@gmail.com>
2025-02-10 16:04:04 +0800

    ss

---
+++ src/views/projects/repayments/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <detail-layout active-tab="repayments">
+    <template #content>
+      <div>fdsafdsa</div>
+    </template>
+  </detail-layout>
+</template>
+
+<script setup>
+  import detailLayout from '../components/detailLayout.vue';
+</script>
+
+<style lang="less" scoped></style>

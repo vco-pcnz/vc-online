4cee560a351c660222680033a853eb7d20826039
ellenging <ellenging@gmail.com>
2025-02-16 16:06:45 +0800

    lm再次检查修改费后退回

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -593,5 +593,7 @@ export default {
   "全部收起": "Collapse all",
   "全部展开": "Expand all",
   "收起": "Collapse",
-  "展开": "Expand"
+  "展开": "Expand",
+  "{0}由{1}修改为了{2}": "{0} was changed from {1} to {2}",
+  "保存后将退回到上一步审核": "After saving, you will return to the previous step for review"
 }

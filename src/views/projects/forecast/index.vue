ce4932c3c0735d5821c75b3dc2238152837e42df
James <1649824587@qq.com>
2025-03-11 20:32:35 +0800

    预估贷款修改

--- src/views/projects/forecast/index.vue
+++ src/views/projects/forecast/index.vue
@@ -3,9 +3,9 @@
     <template #content>
       <a-spin :spinning="loading" size="large">
         <!-- <Cart :data="data"></Cart> -->
-        <div class="flex items-center gap-3">
-          <vco-number :value="projectDetail?.base?.est_loan_money" size="fs_xl" :precision="2"></vco-number>
-          <vco-form-dialog :title="t('修改')" :initData="FormData" :formParams="{ uuid: uuid, estimated_loan: projectDetail?.base?.est_loan_money }" :showTip="true" url="projectDetail/closeCancel1231" @update="loadData">
+        <div class="flex items-center gap-3" v-if="hasPermission('projects:forecast:est_loan_money')">
+          {{ t('预估贷款') }}: <vco-number :value="projectDetail?.base?.est_loan_money" size="fs_xl" :precision="2"></vco-number>
+          <vco-form-dialog :title="t('修改')" :initData="FormData" :formParams="{ uuid: uuid, est_loan_money: projectDetail?.base?.est_loan_money }" :showTip="true" url="project/forecast/updLoanAmount" @update="loadData">
             <a-button type="cyan" size="small" shape="round">{{ t('修改') }}</a-button>
           </vco-form-dialog>
         </div>
@@ -25,6 +25,7 @@ import Cart from './components/cart.vue';
 import tableHook from './components/tableHook.vue';
 import { useRoute } from 'vue-router';
 import { darwdownLog } from '@/api/project/loan';
+import { hasPermission } from '@/directives/permission/index';
 
 const route = useRoute();
 
@@ -55,7 +56,7 @@ const FormData = ref([
   {
     type: 'amount',
     label: '预估贷款',
-    key: 'estimated_loan',
+    key: 'est_loan_money',
     required: true
   }
 ]);

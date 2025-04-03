de21aafd4f0faf03838536b5aa75c3b0bd535a27
James <1649824587@qq.com>
2025-04-03 22:51:21 +0800

    反馈对接

--- src/api/system/index.js
+++ src/api/system/index.js
@@ -1,64 +1,72 @@
-import { request } from "@/utils/request";
+import { request } from '@/utils/request';
 
 // 获取城市信息
 export const systemCity = (params = {}) =>
   request({
-    url: "/system/city/index",
-    method: "get",
+    url: '/system/city/index',
+    method: 'get',
     params
   });
 
 // 利益相关者搜索接口
 export const stakeSelStake = (params = {}) =>
   request({
-    url: "/stake/selStake",
-    method: "get",
+    url: '/stake/selStake',
+    method: 'get',
     params
   });
 
 // 字典
 export const systemDictData = (code) =>
   request({
-    url: "/system/dictData/getDictByCode?code="+code,
-    method: "get"
+    url: '/system/dictData/getDictByCode?code=' + code,
+    method: 'get'
   });
 
-// 
+//
 export const systemConfigData = (params) =>
   request({
-    url: "system/configData/getConfigByCode",
-    method: "get",
+    url: 'system/configData/getConfigByCode',
+    method: 'get',
     params
   });
 
 // 新西兰地址搜索接口
 export const systemCitySearch = (params) =>
   request({
-    url: "system/city/search",
-    method: "get",
+    url: 'system/city/search',
+    method: 'get',
     params
   });
 
 // 新西兰地址搜索接口二段
 export const systemCitySearchInfo = (params) =>
   request({
-    url: "system/city/searchInfo",
-    method: "get",
+    url: 'system/city/searchInfo',
+    method: 'get',
     params
   });
 
 // 获取系统公共信息
 export const indexPub = (params) =>
   request({
-    url: "index/pub",
-    method: "get",
+    url: 'index/pub',
+    method: 'get',
     params
   });
 
 // 获取图片历史
 export const attach = (params) =>
   request({
-    url: "system/attach/index",
-    method: "get",
+    url: 'system/attach/index',
+    method: 'get',
     params
+  });
+
+// 意见反馈
+export const feedback = (data) =>
+  request({
+    url: 'index/feedback',
+    method: 'post',
+    data
   });

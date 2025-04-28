a52c870944af7ca3bd93a5b48338f6c3f48070f6
夏沫凝空 <425523255@qq.com>
2025-04-28 10:15:03 +0800

    1

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -5,13 +5,18 @@
   </div>
   <div class="importBox project">
     <import-export :title="t('项目')" :params="{ type: 'project' }" exportUrl="/temp/project.xls"></import-export>
-    <import-export :title="t('额度费')" :params="{ type: 'linefee' }" exportUrl="/temp/linefee.xls"></import-export>
     <import-export :title="t('减少')" :params="{ type: 'credit' }" exportUrl="/temp/credit.xls"></import-export>
+    <import-export :title="t('LinefeeSchedule')" :params="{ type: 'linefeeSchedule' }" exportUrl="/temp/linefeeSchedule.xls"></import-export>
+    <import-export :title="t('ForecastList')" :params="{ type: 'forecast' }" exportUrl="/temp/forecast.xls"></import-export>
+    <import-export :title="t('Schedule')" :params="{ type: 'linefee' }" exportUrl="/temp/linefee.xls"></import-export>
+    <import-export :title="t('DrawdownApply')" :params="{ type: 'drawdown' }" exportUrl="/temp/drawdown.xls"></import-export>
     <import-export :title="t('抵押物')" :params="{ type: 'securities' }" exportUrl="/temp/securities.xls"></import-export>
-    <import-export :title="t('明细表')" :params="{ type: 'schedule' }" exportUrl="/temp/schedule.xls"></import-export>
-    <import-export :title="t('预测')" :params="{ type: 'forecast' }" exportUrl="/temp/forecast.xls"></import-export>
-    <import-export :title="t('放款')" :params="{ type: 'drawdown' }" exportUrl="/temp/drawdown.xls"></import-export>
+    <import-export :title="t('Close')" :params="{ type: 'close' }" exportUrl="/temp/close.xls"></import-export>
+    <import-export :title="t('projectDo')" :params="{ type: 'projectDo' }" exportUrl="/temp/projectDo.xls"></import-export>
+    <import-export :title="t('cover')" :params="{ type: 'cover' }" exportUrl="/temp/cover.xls"></import-export>
     <import-export :title="t('文件')" :params="{ type: 'document' }" exportUrl="/temp/document.xls"></import-export>
+    <import-export :title="t('condition')" :params="{ type: 'condition' }" exportUrl="/temp/condition.xls"></import-export>
+    <import-export :title="t('projectHistory')" :params="{ type: 'projectHistory' }" exportUrl="/temp/projectHistory.xls"></import-export>
   </div>
 </template>
 

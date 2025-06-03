75a1e6766d6fcf91646c0b9a631fe17d9c4dcf31
James <1649824587@qq.com>
2025-06-04 01:04:51 +0800

    首页接口对接

--- src/views/home/components/QuickAccess/index.vue
+++ src/views/home/components/QuickAccess/index.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="flex justify-between title">
     <div class="bold fs_2xl">{{ t('快捷入口') }}</div>
-    <div class="flex gap-1 items-center cursor-pointer" @click="open = true"><i class="iconfont">&#xe604;</i>{{ t('设置1') }}</div>
+    <div class="flex gap-1 items-center cursor-pointer" @click="setting"><i class="iconfont">&#xe604;</i>{{ t('设置1') }}</div>
   </div>
   <div class="wrapper">
-    <div class="item" v-for="(item, index) in list" :key="index" @click="navigationTo(item.path)">
+    <div class="item" v-for="(item, index) in myList" :key="index" @click="navigationTo(item.path)">
       <img class="icon" :src="item.icon" alt="" />
       <p>{{ item.name }}</p>
     </div>
   </div>
-  <a-modal :width="600" v-if="open" :open="open" :title="t('快捷入口')" @cancel="open = false" class="QuickAccessModal">
+  <a-empty v-if="!myList.length && !loading" style="min-height: 100px" />
+
+  <a-modal :width="600" v-if="open" :open="open" :title="t('快捷入口')" @cancel="open = false" :footer="false" class="QuickAccessModal">
     <div class="bold">{{ t('已添加') }}</div>
     <div class="wrapper">
-      <div class="item" v-for="(item, index) in list" :key="index">
+      <div class="item" v-for="(item, index) in settingData" :key="index">
         <img class="icon" :src="item.icon" alt="" />
         <p>{{ item.name }}</p>
-        <i class="iconfont add reduce">&#xe620;</i>
+        <i class="iconfont add reduce" @click="handReduce(index)">&#xe620;</i>
+      </div>
+      <div style="text-align: center; width: 100%">
+        <a-empty v-if="!settingData.length && !loading" style="min-height: 100px" />
       </div>
     </div>
     <div class="bold">{{ t('列表') }}</div>
@@ -23,57 +28,90 @@
       <div class="item" v-for="(item, index) in list" :key="index">
         <img class="icon" :src="item.icon" alt="" />
         <p>{{ item.name }}</p>
-        <i class="iconfont add">&#xe621;</i>
+        <i class="iconfont add" v-if="showAdd(item)" @click="handAdd(item)">&#xe621;</i>
       </div>
+    </div>
+    <div class="flex justify-center mt-10">
+      <a-button @click="save" type="dark" class="save big uppercase" :loading="loading">
+        {{ t('确认') }}
+      </a-button>
     </div>
   </a-modal>
 </template>
 
 <script setup>
-import { ref, computed, watch, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { navigationTo } from '@/utils/tool';
+import { myQuick, quick, saveMyQuick } from '@/api/home/index';
+import { cloneDeep } from 'lodash';
+
 const { t } = useI18n();
 
 const open = ref(false);
+const loading = ref(false);
+const myList = ref([]);
+const settingData = ref([]);
+const list = ref([]);
 
-const list = ref([
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  },
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  },
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  },
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  },
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  },
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  },
-  {
-    name: 'users',
-    path: '/users/list',
-    icon: 'http://vco.com/uploads/images/annex/20250119/5cd812ff979a3bb1357.png'
-  }
-]);
+const setting = () => {
+  if (!list.value.length) geList();
+  settingData.value = cloneDeep(myList.value);
+  open.value = true;
+};
+
+const getMyQuick = () => {
+  loading.value = ref(true);
+  myQuick()
+    .then((res) => {
+      myList.value = res;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
+
+const geList = () => {
+  loading.value = ref(true);
+  quick()
+    .then((res) => {
+      list.value = res;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
+
+const showAdd = (val) => {
+  return !settingData.value
+    .map((item) => {
+      return item.id;
+    })
+    .includes(val.id);
+};
+
+const handAdd = (val) => {
+  settingData.value.push(val);
+};
+
+const handReduce = (index) => {
+  settingData.value.splice(index, 1);
+};
+const save = () => {
+  loading.value = ref(false);
+  saveMyQuick({ ids: settingData.value.map((d) => d.id) })
+    .then((res) => {
+      getMyQuick()
+      open.value = false
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
+
+onMounted(() => {
+  getMyQuick();
+});
 </script>
 
 <style lang="less" scoped>
@@ -87,7 +125,7 @@ const list = ref([
   padding: 10px 15px;
   gap: 25px;
   .item {
-    width: 65px;
+    width: 94px;
     height: 65px;
     text-align: center;
     cursor: pointer;
@@ -110,7 +148,7 @@ const list = ref([
   .wrapper {
     .item {
       border: 1px solid #dbdbdb;
-      width: 65px;
+      width: 94px;
       height: 65px;
       position: relative;
       .add {

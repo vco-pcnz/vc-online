509f601be46f630a8ec590753317c7999c2849b1
James <1649824587@qq.com>
2025-06-06 17:20:07 +0800

    优化待办

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1003,5 +1003,6 @@ export default {
   "信息": "information",
   "请先设置建筑贷款总额的进度付款信息": "Please set the progress payment information of the build amount",
   "您可以通过还款计算器计算还款金额": "You can calculate the repayment amount through the repayment calculator",
-  "新开贷款": "New loan"
+  "新开贷款": "New loan",
+  "请求1": "Request"
 }

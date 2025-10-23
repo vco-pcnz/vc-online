2ab680e7553106c80abf7fac71444f56b7d3ddf0
James <1649824587@qq.com>
2025-10-23 18:41:30 +0800

    ss

--- src/views/tasks/components/Operation/Project.vue
+++ src/views/tasks/components/Operation/Project.vue
@@ -6,28 +6,33 @@
 import { navigationTo } from '@/utils/tool';
 
 const todoHandle = (data) => {
+  console.log(data.process_type);
   if (data.process_type === 'drawdown') {
-    navigationTo(`/projects/drawdowns?uuid=${data.uuid}`,true);
+    navigationTo(`/projects/drawdowns?uuid=${data.uuid}`, true);
   } else if (data.process_type === 'repayment') {
-    navigationTo(`/projects/repayments?uuid=${data.uuid}`,true);
+    navigationTo(`/projects/repayments?uuid=${data.uuid}`, true);
   } else if (data.process_type === 'discharge') {
-    navigationTo(`/projects/discharge?uuid=${data.uuid}`,true);
+    navigationTo(`/projects/discharge?uuid=${data.uuid}`, true);
+  } else if (data.process_type === 'security-edit') {
+    navigationTo(`/projects/discharge?uuid=${data.uuid}&type=3`, true);
+  } else if (data.process_type === 'discharge-security') {
+    navigationTo(`/projects/discharge?uuid=${data.uuid}&type=4`, true);
   } else if (data.process_type === 'security') {
-    navigationTo(`/projects/discharge?uuid=${data.uuid}&type=1`,true);
+    navigationTo(`/projects/discharge?uuid=${data.uuid}&type=2`, true);
   } else if (['penalty-start', 'penalty-end'].includes(data.process_type)) {
-    navigationTo(`/projects/penalty?uuid=${data.uuid}`,true);
+    navigationTo(`/projects/penalty?uuid=${data.uuid}`, true);
   } else if (data.process_type === 'variation') {
     if (data.relation_id) {
-      navigationTo(`/projects/variations-details/about?uuid=${data.uuid}&id=${data.relation_id}`,true);
+      navigationTo(`/projects/variations-details/about?uuid=${data.uuid}&id=${data.relation_id}`, true);
     } else {
-      navigationTo(`/projects/variations?uuid=${data.uuid}`,true);
+      navigationTo(`/projects/variations?uuid=${data.uuid}`, true);
     }
   } else if (data.process_type === 'journal') {
-    navigationTo(`/projects/journal?uuid=${data.uuid}`,true);
+    navigationTo(`/projects/journal?uuid=${data.uuid}`, true);
   } else if (['closed', 'closed-cancel', 'reopen'].includes(data.process_type)) {
-    navigationTo(`/projects/about?uuid=${data.uuid}`,true);
+    navigationTo(`/projects/about?uuid=${data.uuid}`, true);
   } else if (data.process_type === 'aml') {
-    navigationTo(`/projects/orgs?uuid=${data.uuid}&type=2`,true);
+    navigationTo(`/projects/orgs?uuid=${data.uuid}&type=2`, true);
   }
 };
 

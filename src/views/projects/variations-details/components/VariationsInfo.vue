a9fcf8c896618fc0136a5483b6429133b350afdb
ellenging <ellenging@gmail.com>
2025-11-28 11:55:48 +0800

    变更 展示修改

--- src/views/projects/variations-details/components/VariationsInfo.vue
+++ src/views/projects/variations-details/components/VariationsInfo.vue
@@ -274,6 +274,10 @@ const changeCostAfter = computed(() => {
   if ([2, 5].includes(Number(variationsInfo.value.type))) {
     resNum = tool.minus(props.detail?.credit?.right?.devCost || 0, changeNum)
   }
+
+  if ([3].includes(Number(variationsInfo.value.type))) {
+    resNum = props.detail?.credit?.right?.devCost || 0
+  }
   return resNum;
 });
 

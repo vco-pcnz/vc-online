1f208384f1fce4ed453f2739f28b47105e436bd8
James <1649824587@qq.com>
2025-12-09 15:13:24 +0800

    进件外部用户提交增加提示语，去点项目详情开发成本显示

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1300,5 +1300,7 @@ export default {
   "贷款期限": "Loan duration",
   "预计还款今天": "Est. payoff (today)",
   "已提取金额": "Drawn amount",
-  "可提取金额": "Available to draw"
+  "可提取金额": "Available to draw",
+  "申请已提交": "Application Submitted",
+  "感谢您！您的贷款申请已提交成功。我们的贷款团队将尽快审核。": "Thank you! Your loan application has been successfully submitted. Our Lending Team will review it shortly."
 }

b0ad2cd4ee6278f3ca4c280db5b0b98a42b7eed8
James <1649824587@qq.com>
2026-01-12 09:45:22 +0800

    ss

--- src/components/schedule/index.vue
+++ src/components/schedule/index.vue
@@ -589,7 +589,7 @@ const adSubmitRequest = () => {
   adLoading.value = true;
   const params = {
     uuid: props.currentId,
-    pdf: props.isClose ? 3 : 2
+    pdf: 2
   };
   // if (!props.isClose) {
     params.s_date = adFormState.s_date ? dayjs(adFormState.s_date).format('YYYY-MM-DD') : '';

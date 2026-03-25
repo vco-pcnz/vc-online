ea1a8db3d4b1b2df6113524d2b63d7a323a32e5a
James <1649824587@qq.com>
2026-03-25 15:22:48 +0800

    vsl 放款日期限制到买断之后

--- src/views/projects/repayments/components/TableBlock.vue
+++ src/views/projects/repayments/components/TableBlock.vue
@@ -100,7 +100,7 @@ const viewDetail = (val) => {
 const setStatusColor = (val) => {
   if (val.state == 1000 && val.status > 1) {
     return '#272727';
-  } else if (val.state == -900) {
+  } else if (val.state == -900 || val.state == -100) {
     return '#ff7875';
   } else {
     return '#d3a631';

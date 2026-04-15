d244b3d4dab7c95684de8fa53b2b11f554de33f8
James <1649824587@qq.com>
2026-04-15 15:53:36 +0800

    ss

--- src/views/investor/home/components/SatisticsVsl/Search.vue
+++ src/views/investor/home/components/SatisticsVsl/Search.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <vco-page-search @keyup.enter="searchHandle(false)">
-      <vco-page-search-item :title="t('日期')" width="280">
-        <a-range-picker
-          v-model:value="searchForm.range"
-          style="width: 100%"
-          :format="selectDateFormat()"
-          :disabled-date="disabledDateAfterToday"
-          :placeholder="[t('开始日期'), t('结束日期')]"
-        />
+      <vco-page-search-item :title="t('开始日期1')" width="266">
+        <div class="flex items-center gap-2">
+          <a-date-picker v-model:value="searchForm.start_date_s" :format="selectDateFormat()" :placeholder="t('开始日期2')" @change="searchForm.start_date_e = ''" />
+          <p>-</p>
+          <a-date-picker v-model:value="searchForm.start_date_e" :format="selectDateFormat()" :disabledDate="disabledDateFormatAfter" :placeholder="t('结束日期2')" />
+        </div>
+      </vco-page-search-item>
+      <vco-page-search-item :title="t('关账日期')" width="266">
+        <div class="flex items-center gap-2">
+          <a-date-picker v-model:value="searchForm.close_date_s" :format="selectDateFormat()" :placeholder="t('开始日期2')" @change="searchForm.close_date_e = ''" />
+          <p>-</p>
+          <a-date-picker v-model:value="searchForm.close_date_e" :format="selectDateFormat()" :disabledDate="disabledDateFormatAfter2" :placeholder="t('结束日期2')" />
+        </div>
       </vco-page-search-item>
       <vco-page-search-item   width="180">
         <div class="flex justify-between items-end gap-2">
@@ -27,26 +32,49 @@ import { ref } from 'vue';
 import dayjs from 'dayjs';
 import { useI18n } from 'vue-i18n';
 import { selectDateFormat } from '@/utils/tool';
+import { cloneDeep } from 'lodash';
 
 const emits = defineEmits(['search']);
 
 const { t } = useI18n();
 
 const searchForm = ref({
-  range: null
+  start_date_s: '',
+  start_date_e: '',
+  close_date_s: '',
+  close_date_e: ''
 });
 
-/** 仅允许选择今天及之前的日期 */
-const disabledDateAfterToday = (current) => current && current.isAfter(dayjs(), 'day');
+const disabledDateFormatAfter = (current) => {
+  const targetDate = new Date(searchForm.value.start_date_s).setHours(0, 0, 0, 0);
+  return current && current < targetDate;
+};
 
+const disabledDateFormatAfter2 = (current) => {
+  const targetDate = new Date(searchForm.value.close_date_s).setHours(0, 0, 0, 0);
+  return current && current < targetDate;
+};
 const searchHandle = (flag) => {
   if (flag) {
-    searchForm.value.range = null;
+    searchForm.value.start_date_s = '';
+    searchForm.value.start_date_e = '';
+    searchForm.value.close_date_s = '';
+    searchForm.value.close_date_e = '';
   }
-  const [start, end] = searchForm.value.range || [];
-  const sday = start ? dayjs(start).format('YYYY-MM-DD') : '';
-  const eday = end ? dayjs(end).format('YYYY-MM-DD') : '';
-  emits('search', { sday, eday });
+  const data = cloneDeep(searchForm.value);
+  if (data.start_date_s) {
+    data.start_date_s = dayjs(data.start_date_s).format('YYYY-MM-DD');
+  }
+  if (data.start_date_e) {
+    data.start_date_e = dayjs(data.start_date_e).format('YYYY-MM-DD');
+  }
+  if (data.close_date_s) {
+    data.close_date_s = dayjs(data.close_date_s).format('YYYY-MM-DD');
+  }
+  if (data.close_date_e) {
+    data.close_date_e = dayjs(data.close_date_e).format('YYYY-MM-DD');
+  }
+  emits('search', data);
 };
 
 </script>
